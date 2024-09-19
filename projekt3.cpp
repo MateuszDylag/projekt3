bdacf7ceffdd983d87b951f602ab5e4089039c3c
Mateusz Dyląg <mateusz@gmail.com>
2024-09-19 10:45:49 +0200

    Dodano funkcję sprawdzającą czy liczba jest pierwsza

--- projekt3.cpp
+++ projekt3.cpp
@@ -2,18 +2,36 @@
 
 using namespace std;
 
+bool isPrime(int a) {
+    if(a < 2) {
+        return false;
+    }
+    for(int i = 2; i <= a / 2; i++) {
+        if(a % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
     int wyjscie;
     do {
-        cout << endl;
-        cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Silnia" << endl;
+        cout << "2. Czy pierwsza" << endl;
         cin >> wyjscie;
+
+        if(wyjscie == 2) {
+            cout << "Wpisz liczbe by sprawdzic czy jest pierwsza: ";
+            cin >> a;
+
+            isPrime(a) ? cout << "Pierwsza" << endl : cout << "Zlozona" << endl;
+        }
+
     } while(wyjscie != 0);
     return 0;
 }
