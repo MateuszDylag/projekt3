14e6b310a539974460a1ae6c5050e9b0a62f0101
Mateusz Dyląg <mateusz@gmail.com>
2024-09-19 10:43:24 +0200

    Dodano funkcję obliczającą silnię

--- projekt3.cpp
+++ projekt3.cpp
@@ -2,18 +2,30 @@
 
 using namespace std;
 
+int factorial(int a) {
+    if(a == 0) {
+        return 1;
+    }
+    return a * factorial(a - 1);
+}
+
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
     int wyjscie;
     do {
-        cout << endl;
-        cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Silnia" << endl;
         cin >> wyjscie;
+
+        if(wyjscie == 1) {
+            cout << "Wpisz liczbe z ktorej zostanie policzona silnia: ";
+            cin >> a;
+
+            cout << "Wynik: " << factorial(a) << endl;
+        }
+
     } while(wyjscie != 0);
     return 0;
 }
